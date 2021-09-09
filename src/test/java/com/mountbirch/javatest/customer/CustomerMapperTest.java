0523f12daa65fec135981f4fa246a678450a726a
silver <silver.palumaa@mountbirch.com>
2021-09-09 14:42:07 +0300

    Added CustomerMapper initialization to CustomerMapperTest

---
+++ src/test/java/com/mountbirch/javatest/customer/CustomerMapperTest.java
@@ -0,0 +1,16 @@
+package com.mountbirch.javatest.customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerMapperTest {
+
+    private CustomerMapper mapper = new CustomerMapperImpl();
+
+    @Test
+    void fromEntity_shouldMapCustomerToDTO_successfully() {
+        // TODO implementation here
+    }
+
+}
